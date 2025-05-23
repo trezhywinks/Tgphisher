cd88d20a46964c9ba5089b7954dd387c2739e65b
trezhywinks <dreqxyonion@gmail.com>
2025-05-23 19:02:04 -0300

    Tgphisher

--- Tgphisher.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-wget https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-amd64
-
-mv cloudflared-linux-amd64 cloudflared
-chmod +x cloudflared
-
-sudo mv cloudflared /usr/local/bin/
