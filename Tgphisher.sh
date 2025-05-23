3036a2d038400ad7dda1db81c241436c1dbfc821
trezhywinks <dreqxyonion@gmail.com>
2025-05-23 15:55:48 -0300

    Tgphisher

--- Tgphisher.sh
+++ Tgphisher.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-node main.js
+wget https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-amd64
+
+mv cloudflared-linux-amd64 cloudflared
+chmod +x cloudflared
+
+sudo mv cloudflared /usr/local/bin/
