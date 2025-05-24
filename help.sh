83a952ecbd275db76a106ae60d32353f6163d0b9
trezhywinks <dreqxyonion@gmail.com>
2025-05-24 10:13:26 -0300

    Tgphisher

--- help.sh
+++ help.sh
@@ -3,7 +3,7 @@ sudo cp -r tgphisher /bin/
 
 RED="$(printf '\033[31m')"  WHITE="$(printf '\033[37m')"
 ORG="$(printf '\e[1;93m Werbot Ver : 1.0')"
-REF="$(printf ' \e[1;37m\n')"
+REF="$(printf ' \e[1;37m\n')" BLACK="$(printf '\033[30m')"
 
 printf " \e[1;37m\n"
 
@@ -28,14 +28,17 @@ ${WHITE}| Github       :${RED} https://github.com/trezhywinks${WHITE}
  
 [::] Tgphisher (commands) 
  
-tgphisher -s --pisher         : Start the phisher script to get the
-                                victim's data.
+${BLACK}┌──${WHITE}tgphisher -s --pisher 
+${BLACK}└─ Start the phisher script to get the                                
+   victim's data.${WHITE}
 
-tgphisher -d session.txt      : Delete the old session if you want
-                                to start a new session.
+${BLACK}┌──${WHITE}tgphisher -d session.txt
+${BLACK}└─ Delete the old session if you want
+   to start a new session.${WHITE}
 
-tgphisher -s --telegram       : Start the script to connect with the
-                                victim's telegram.
+${BLACK}┌──${WHITE}tgphisher -s --telegram
+${BLACK}└─ Start the script to connect with the
+   victim's telegram.${WHITE}
 EOF
 }
 
